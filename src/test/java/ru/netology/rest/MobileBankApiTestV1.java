f3243787a5ba4d0059f7aed07d17f69fc1a32c4e
Daniil Salikov <denielnit@gmail.com>
2026-01-13 16:55:38 +0300

    New CashBackHacker repos

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,29 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .body("", hasSize(3))
+                .body("[1].id", equalTo(2))
+                .body("[1].currency", equalTo("USD"))
+                .body("[1].balance", greaterThan(0));
+    }
+}
